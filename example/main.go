4a0e0e23a0460ff52fbdc014b793253ac49598f1
Raffaele Sena <raff367@gmail.com>
2022-01-14 13:23:08 -0800

    Added example

---
+++ example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/raff/go-tesseract"
+)
+
+func main() {
+	imagebytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tess := tesseract.BaseAPICreate()
+	defer func() {
+		tess.Clear()
+		tess.End()
+	}()
+
+	if ret := tess.Init3("", "eng"); ret != 0 {
+		log.Fatal("Tesseract init", ret)
+	}
+
+	// imagebytes should be a byte buffer
+	// containing an image in a format supported by leptonica (i.e. png)
+	pbytes := tess.SetImageBytes(imagebytes)
+
+	if text := tess.GetUTF8Text(); text != "" {
+		fmt.Println(text)
+	}
+
+	tesseract.FreeImageBytes(pbytes)
+}
